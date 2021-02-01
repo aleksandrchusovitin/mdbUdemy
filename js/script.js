9546b0f525d2b1a0e6bf0d1d2925bce340e3d5bd
Alexandr Chusovitin <chusovitinalexandr@gmail.com>
2021-02-01 23:06:54 +0700

    dzdom start

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту
-
-5) Добавить нумерацию выведенных фильмов */
-
 "use strict";
 
 const movieDB = {
@@ -23,20 +9,51 @@ const ads = promo.querySelectorAll(".promo__adv img");
 const genre = promo.querySelector(".promo__genre");
 const bg = promo.querySelector(".promo__bg");
 const list = promo.querySelector(".promo__interactive-list");
+const form = promo.querySelector(".add");
+const formInput = form.querySelector(".adding__input");
+const formCheckbox = form.querySelector("input[type=checkbox]");
+const formButton = form.querySelector("button");
+const filmDelete = promo.querySelector(".delete");
 
 ads.forEach((img) => {
   img.remove();
 });
+
 genre.textContent = "драма";
-bg.style.background = "url('../img/bg.jpg') center center/cover no-repeat";
+bg.style.background = "url(../img/bg.jpg) center center/cover no-repeat";
 
-list.innerHTML = "";
-movieDB.movies.sort();
+// Создаем функцию для обработки события "нажатие на кнопку формы"
+function handler(event) {
+  event.preventDefault();
 
-movieDB.movies.forEach((film, i) => {
-  list.innerHTML += `
+  let film = formInput.value;
+  if (film.length > 21) {
+    film = `${film.slice(0, 21)}...`;
+  }
+
+  if (formCheckbox.checked) {
+    console.log("Добавляем любимый фильм");
+  }
+
+  movieDB.movies.push(film);
+  createFilmList();
+}
+
+// Вешаем обработчик событий на кнопку
+formButton.addEventListener("click", handler);
+
+// Функция для создания в разметке списка фильмов из базы данных
+function createFilmList() {
+  list.innerHTML = "";
+  movieDB.movies.sort();
+
+  movieDB.movies.forEach((film, i) => {
+    list.innerHTML += `
     <li class="promo__interactive-item">${i + 1} ${film}
       <div class="delete"></div>
     </li>
   `;
-});
+  });
+}
+
+createFilmList();
